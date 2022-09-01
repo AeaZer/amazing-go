49a893f2dc70d5b259eefb1efa55cbbb10b8a459
AeaZer <3474974468@qq.com>
2022-09-01 10:24:57 +0800

    Assign i++

---
+++ syntax/syntax.go
@@ -0,0 +1,3 @@
+package syntax
+
+// empty file
---
+++ syntax/syntax_test.go
@@ -0,0 +1,15 @@
+package syntax
+
+import "testing"
+
+func TestMain(m *testing.M) {
+	m.Run()
+}
+
+func TestSyntaxSuger(t *testing.T) {
+	var a int
+	// a := i++
+
+	t.Log(a)
+	// syntax_test.go:11: compile field
+}
